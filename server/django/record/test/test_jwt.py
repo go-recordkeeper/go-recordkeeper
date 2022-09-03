40a3d66da0b3ff331e73b00f2f5a50faa212f771
Daniel Chiquito <daniel.chiquito@gmail.com>
2022-09-03 11:14:41 -0400

    Test logging in with a token

--- server/django/record/test/test_jwt.py
+++ server/django/record/test/test_jwt.py
@@ -30,3 +30,14 @@ def test_get_user(authenticated_client, user):
 def test_get_user_unauthenticated(client, user):
     response = client.get('/user/')
     assert response.status_code == 403
+
+@pytest.mark.django_db
+def test_login_with_token(authenticated_client, user):
+    response = authenticated_client.post(
+        '/login/', {'username': 'John Doe', 'password': 'hunter12'}, content_type='application/json',
+    )
+    assert response.status_code == 200
+    token = response.json()
+    payload = jwt.decode(token, key=settings.SECRET_KEY, algorithms='HS256')
+    # If no exception, it's a valid token
+    assert payload['id'] == user.id
