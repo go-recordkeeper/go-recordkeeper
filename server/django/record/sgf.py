fb47f19027ba566ac9f317488ee58f3ef254ccb9
Daniel Chiquito <daniel.chiquito@gmail.com>
2022-09-09 12:27:51 -0400

    Export all SGF metadata

--- server/django/record/sgf.py
+++ server/django/record/sgf.py
@@ -5,6 +5,16 @@ from record.models import Record
 
 def export_sgf(record: Record) -> bytes:
     game = sgf.Sgf_game(size=record.board_size)
+    root_node = game.get_root()
+    if record.name:
+        root_node.set('GN', record.name)
+    if record.comment:
+        root_node.set('GC', record.name)
+    root_node.set('PB', record.black_player)
+    root_node.set('PW', record.white_player)
+    root_node.set('KM', record.komi)
+    root_node.set('HA', record.handicap)
+
     for move in record.moves.all():
         node = game.extend_main_sequence()
         # position increments left to right, then top to bottom
