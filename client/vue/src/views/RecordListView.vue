cd2785d10edee921f333468375939871f814863f
Daniel Chiquito <daniel.chiquito@gmail.com>
2022-09-10 14:16:16 -0400

    Large scale board list formatting

--- client/vue/src/views/RecordListView.vue
+++ client/vue/src/views/RecordListView.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { DocumentArrowDownIcon, PencilIcon, TrashIcon } from '@heroicons/vue/24/outline';
 import Client from '@/client';
 import type { Record } from '@/client';
 import type { Ref } from 'vue';
@@ -12,7 +13,7 @@ client.getRecords().then((rs) => {
 });
 
 async function newRecord() {
-    await router.push({name: 'create'});
+    await router.push({ name: 'create' });
 }
 
 async function deleteRecord(id: number) {
@@ -26,15 +27,43 @@ async function downloadRecord(id: number) {
 </script>
 
 <template>
-    <div class="about">
-        <h1>These are your game records</h1>
-        <div v-for="record of records" :key="record.id">
-            <router-link :to="{ name: 'record', params: { id: record.id } }">
-                record {{ record.id }}
-            </router-link>
-            <button @click="deleteRecord(record.id)">delete me</button>
-            <button @click="downloadRecord(record.id)">SGF</button>
-        </div>
-        <button @click="newRecord">Make a new record</button>
+    <div>
+        <div class="text-xl m-4">Games</div>
+        <table class="table-auto w-full">
+            <tbody>
+                <tr v-for="record of records" :key="record.id" class="border-b">
+                    <td class="p-4">
+                        <router-link :to="{ name: 'record', params: { id: record.id } }">
+                            {{ record.name }}
+                        </router-link>
+                    </td>
+                    <td class="p-4">
+                        <router-link :to="{ name: 'record', params: { id: record.id } }">
+                            {{ record.black_player }} vs. {{ record.white_player }}
+                        </router-link>
+                    </td>
+                    <td class="p-4">
+                        <router-link :to="{ name: 'record', params: { id: record.id } }">
+                            {{ new Date(record.created).toLocaleString() }}
+                        </router-link>
+                    </td>
+                    <td class="p-4">
+                        <button @click="downloadRecord(record.id)">
+                            <DocumentArrowDownIcon class="block h-6 w-6" />
+                        </button>
+                    </td>
+                    <td class="p-4">
+                        <RouterLink :to="{ name: 'update', params: { id: record.id } }">
+                            <PencilIcon class="block h-6 w-6" />
+                        </RouterLink>
+                    </td>
+                    <td class="p-4">
+                        <button @click="deleteRecord(record.id)">
+                            <TrashIcon class="block h-6 w-6" />
+                        </button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </template>
