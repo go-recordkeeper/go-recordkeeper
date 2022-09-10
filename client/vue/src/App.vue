e6da265f52addae079d5a6dfef86a153ed564b33
Daniel Chiquito <daniel.chiquito@gmail.com>
2022-09-10 13:19:47 -0400

    Fully functional mobile nav

--- client/vue/src/App.vue
+++ client/vue/src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
+import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue';
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/vue/24/outline';
 import { provide, ref } from 'vue';
 import type { Ref } from 'vue';
 import { RouterLink, RouterView } from 'vue-router'
@@ -72,36 +73,20 @@ client.getCurrentUser().then((user) => {
         </div>
       </div>
 
-      <DisclosurePanel class="md:hidden">
+      <DisclosurePanel class="md:hidden" v-slot="{ close }">
         <div class="space-y-1 px-2 pt-2 pb-3 sm:px-3">
-          <!-- <DisclosureButton v-for="item in navigation" :key="item.name" as="a" :href="item.href"
-            :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block px-3 py-2 rounded-md text-base font-medium']"
-            :aria-current="item.current ? 'page' : undefined">{{ item.name }}</DisclosureButton> -->
-        </div>
-        <div class="border-t border-gray-700 pt-4 pb-3">
-          <div class="flex items-center px-5">
-            <div class="flex-shrink-0">
-              <!-- <img class="h-10 w-10 rounded-full" :src="user.imageUrl" alt="" /> -->
-            </div>
-            <div class="ml-3">
-              <div class="text-base font-medium leading-none text-white">user.name</div>
-              <div class="text-sm font-medium leading-none text-gray-400">user.email</div>
-            </div>
-            <button type="button"
-              class="ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-              <span class="sr-only">View notifications</span>
-              <BellIcon class="h-6 w-6" aria-hidden="true" />
-            </button>
-          </div>
-          <div class="mt-3 space-y-1 px-2">
-            <!-- <DisclosureButton v-for="item in userNavigation" :key="item.name" as="a" :href="item.href"
-              class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">
-              {{ item.name }}</DisclosureButton> -->
-          </div>
+          <RouterLink :to="{ 'name': 'records' }" v-if="userRef" @click="close"
+            class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
+            Games
+          </RouterLink>
+          <RouterLink :to="{ 'name': 'create' }" v-if="userRef" @click="close"
+            class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
+            New Game
+          </RouterLink>
+          <AuthWidget :closeDialog="close"/>
         </div>
       </DisclosurePanel>
     </Disclosure>
-    <AuthWidget />
   </div>
 
   <RouterView />
--- client/vue/src/components/AuthWidget.vue
+++ client/vue/src/components/AuthWidget.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
-import type { Ref } from 'vue';
+import type { PropType, Ref } from 'vue';
 import type { User } from '@/client';
 import { inject } from 'vue';
 import { RouterLink } from 'vue-router'
 import Client from '@/client';
+
+const props = defineProps({
+    closeDialog: Function as PropType<() => {}>,
+})
 
 let user = inject<Ref<User | null>>('user') as Ref<User | null>;
 let client = new Client();
@@ -11,21 +15,47 @@ let client = new Client();
 function logout() {
     client.logout();
     user.value = null;
+    pleaseCloseDialog();
+}
+
+function pleaseCloseDialog() {
+    if (props.closeDialog) {
+        props.closeDialog();
+    }
 }
 </script>
 
 <template>
     <div v-if="!user">
-        <RouterLink :to="{ 'name': 'login' }"
-            class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Log in
-        </RouterLink>
+        <div class="hidden md:block">
+            <RouterLink :to="{ 'name': 'login' }" @click="pleaseCloseDialog"
+                class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Log in
+            </RouterLink>
+        </div>
+        <div class="md:hidden">
+            <RouterLink :to="{ 'name': 'login' }" @click="pleaseCloseDialog"
+                class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Log in
+            </RouterLink>
+        </div>
     </div>
     <div v-else>
-        <span class="text-gray-300 px-3 py-2 rounded-md text-sm font-medium">
-            Welcome {{user.username}}!
-        </span>
-        <button @click="logout" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
-            Log out
-        </button>
+        <div class="hidden md:block">
+            <span class="text-gray-300 px-3 py-2 rounded-md text-sm font-medium">
+                Welcome {{user.username}}!
+            </span>
+            <button @click="logout"
+                class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                Log out
+            </button>
+        </div>
+        <div class="md:hidden">
+            <span class="text-gray-300 block px-3 py-2 rounded-md text-base font-medium">
+                Welcome {{user.username}}!
+            </span>
+            <button @click="logout"
+                class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
+                Log out
+            </button>
+        </div>
     </div>
 </template>
