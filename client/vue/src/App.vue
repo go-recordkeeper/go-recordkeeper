47d9740f9d7e765fd14d7b8704aaf8e2ffde9b20
Daniel Chiquito <daniel.chiquito@gmail.com>
2022-09-09 22:02:56 -0400

    half-ass nav

--- client/vue/src/App.vue
+++ client/vue/src/App.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
 import { provide, ref } from 'vue';
 import type { Ref } from 'vue';
 import { RouterLink, RouterView } from 'vue-router'
@@ -28,79 +29,105 @@ client.getCurrentUser().then((user) => {
     </div>
   </header> -->
 
-  <nav>
-    <!-- <RouterLink :to="{'name': 'login'}">Log in</RouterLink> -->
-    <RouterLink :to="{ 'name': 'records' }">All Game Records</RouterLink>
+  <div class="min-h-full">
+    <Disclosure as="nav" class="bg-gray-800" v-slot="{ open }">
+      <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div class="flex h-16 items-center justify-between">
+          <div class="flex items-center">
+            <div class="flex-shrink-0">
+              <img class="h-8 w-8" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                alt="Your Company" />
+            </div>
+            <div class="hidden md:block">
+              <div class="ml-10 flex items-baseline space-x-4">
+                <!-- <a v-for="item in navigation" :key="item.name" :href="item.href"
+                  :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'px-3 py-2 rounded-md text-sm font-medium']"
+                  :aria-current="item.current ? 'page' : undefined">{{ item.name }}</a> -->
+                <RouterLink :to="{ 'name': 'records' }"
+                  class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                  All Game Records
+                </RouterLink>
+              </div>
+            </div>
+          </div>
+          <div class="hidden md:block">
+            <div class="ml-4 flex items-center md:ml-6">
+              <button type="button"
+                class="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white text-lg">
+                <span class="sr-only">View notifications</span>
+                <!-- <BellIcon class="h-6 w-6" aria-hidden="true" /> -->
+                +
+              </button>
+
+              <!-- Profile dropdown -->
+              <Menu as="div" class="relative ml-3">
+                <div>
+                  <MenuButton
+                    class="flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                    <span class="sr-only">Open user menu</span>
+                    <!-- <img class="h-8 w-8 rounded-full" :src="user.imageUrl" alt="" /> -->
+                  </MenuButton>
+                </div>
+                <transition enter-active-class="transition ease-out duration-100"
+                  enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100"
+                  leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100"
+                  leave-to-class="transform opacity-0 scale-95">
+                  <MenuItems
+                    class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <!-- <MenuItem v-for="item in userNavigation" :key="item.name" v-slot="{ active }">
+                    <a :href="item.href"
+                      :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">{{ item.name
+                      }}</a>
+                    </MenuItem> -->
+                    <RouterLink :to="{ 'name': 'records' }">All Game Records</RouterLink>
+                  </MenuItems>
+                </transition>
+              </Menu>
+            </div>
+          </div>
+          <div class="-mr-2 flex md:hidden">
+            <!-- Mobile menu button -->
+            <DisclosureButton
+              class="inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+              <span class="sr-only">Open main menu</span>
+              <Bars3Icon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
+              <XMarkIcon v-else class="block h-6 w-6" aria-hidden="true" />
+            </DisclosureButton>
+          </div>
+        </div>
+      </div>
+
+      <DisclosurePanel class="md:hidden">
+        <div class="space-y-1 px-2 pt-2 pb-3 sm:px-3">
+          <!-- <DisclosureButton v-for="item in navigation" :key="item.name" as="a" :href="item.href"
+            :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block px-3 py-2 rounded-md text-base font-medium']"
+            :aria-current="item.current ? 'page' : undefined">{{ item.name }}</DisclosureButton> -->
+        </div>
+        <div class="border-t border-gray-700 pt-4 pb-3">
+          <div class="flex items-center px-5">
+            <div class="flex-shrink-0">
+              <!-- <img class="h-10 w-10 rounded-full" :src="user.imageUrl" alt="" /> -->
+            </div>
+            <div class="ml-3">
+              <div class="text-base font-medium leading-none text-white">user.name</div>
+              <div class="text-sm font-medium leading-none text-gray-400">user.email</div>
+            </div>
+            <button type="button"
+              class="ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+              <span class="sr-only">View notifications</span>
+              <BellIcon class="h-6 w-6" aria-hidden="true" />
+            </button>
+          </div>
+          <div class="mt-3 space-y-1 px-2">
+            <!-- <DisclosureButton v-for="item in userNavigation" :key="item.name" as="a" :href="item.href"
+              class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">
+              {{ item.name }}</DisclosureButton> -->
+          </div>
+        </div>
+      </DisclosurePanel>
+    </Disclosure>
     <AuthWidget />
-  </nav>
+  </div>
 
   <RouterView />
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
-
-function loadUser(): any {
-  throw new Error('Function not implemented.');
-}
