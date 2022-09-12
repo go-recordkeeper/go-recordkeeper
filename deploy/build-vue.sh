8766a9ff10396ccd6c95b60b35bd9c7694ad3c65
Daniel Chiquito <daniel.chiquito@gmail.com>
2022-09-12 17:47:11 -0400

    Lots of tweaks to get files + API hosting

--- deploy/build-vue.sh
+++ deploy/build-vue.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir -p dist/static
+mkdir -p dist
 
-docker run --rm -v $(pwd)/goban-vue:/goban-vue -w /goban-vue -v $(pwd)/dist/static:/outDir node npm install && \
-docker run --rm -v $(pwd)/goban-vue:/goban-vue -w /goban-vue -v $(pwd)/dist/static:/outDir node npm run build-only -- --emptyOutDir --outDir /outDir --base /static/
+docker run --rm -v $(pwd)/goban-vue:/goban-vue -w /goban-vue -v $(pwd)/dist:/outDir node npm install && \
+docker run --rm -v $(pwd)/goban-vue:/goban-vue -w /goban-vue -v $(pwd)/dist:/outDir node npm run build-only -- --emptyOutDir --outDir /outDir --assetsDir static
