08f31f9533b08c77992762fd7e3c08f251141c63
Daniel Chiquito <daniel.chiquito@gmail.com>
2022-09-14 09:10:49 -0400

    Home page copy

--- client/vue/src/views/HomeView.vue
+++ client/vue/src/views/HomeView.vue
@@ -2,7 +2,34 @@
 </script>
 
 <template>
-  <div>
-    Welcome!
+  <div class="mx-auto max-w-3xl">
+    <div class="my-10 text-center text-4xl">
+      Record your Go games!
+    </div>
+    <div class="my-5 text-center text-lg">
+      I made this app so that I would have a convenient place to record my <a href="https://en.wikipedia.org/wiki/Go_(game)" class="text-blue-800 underline">Go</a> games on my phone.
+    </div>
+    <div class="my-5 text-center text-lg">
+      You're welcome to make an account and try it out as well!
+    </div>
+    <div class="my-5 text-center text-lg">
+      Just remember that this website is running on a computer in my house, and I make no promises as to the permanence of your data.
+    </div>
+    <div class="my-5">&nbsp;</div>
+    <div class="my-5 text-center text-lg">
+      Source code can be found at:
+    </div>
+    <div class="my-5 text-center text-lg">
+      Goban widget: <a href="https://github.com/dchiquito/go-board" class="text-blue-800 underline">https://github.com/dchiquito/go-board</a>
+    </div>
+    <div class="my-5 text-center text-lg">
+      Server: <a href="https://github.com/dchiquito/go-board" class="text-blue-800 underline">https://github.com/dchiquito/goban-python</a>
+    </div>
+    <div class="my-5 text-center text-lg">
+      Web client: <a href="https://github.com/dchiquito/go-board" class="text-blue-800 underline">https://github.com/dchiquito/goban-vue</a>
+    </div>
+    <div class="my-5 text-center text-lg">
+      Deployment: <a href="https://github.com/dchiquito/go-board" class="text-blue-800 underline">https://github.com/dchiquito/goban-deploy</a>
+    </div>
   </div>
 </template>
