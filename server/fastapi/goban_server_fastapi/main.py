9f7d9c37e3570c7f7608984a8a1299b2af66ba90
Daniel Chiquito <daniel.chiquito@gmail.com>
2022-09-21 13:59:11 -0400

    Return 400 on validation errors

--- server/fastapi/goban_server_fastapi/main.py
+++ server/fastapi/goban_server_fastapi/main.py
@@ -1,7 +1,11 @@
 from typing import Union
 
 from fastapi import FastAPI
+from fastapi.exceptions import RequestValidationError
 from pydantic import BaseModel
+from starlette import status
+from starlette.requests import Request
+from starlette.responses import JSONResponse, Response
 
 from goban_server_fastapi.auth import PBKDF2PasswordHasher, generate_token
 from goban_server_fastapi.models import get_user
@@ -10,7 +14,23 @@ class LoginRequest(BaseModel):
     username: str
     password: str
 
+async def request_validation_exception_handler(
+    request: Request, exc: RequestValidationError
+) -> JSONResponse:
+    print(exc.errors())
+    fields = {}
+    for error in exc.errors():
+        field = error['loc'][1]
+        if field not in fields:
+            fields[field] = []
+        fields[field].append(error['msg'])
+    return JSONResponse(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        content=fields,
+    )
+
 app = FastAPI()
+app.add_exception_handler(RequestValidationError, request_validation_exception_handler)
 
 
 @app.post('/api/login', status_code=200)
