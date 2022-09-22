271ae611754a956077147ffe85f94075896bbec9
Daniel Chiquito <daniel.chiquito@gmail.com>
2022-09-21 21:17:51 -0400

    Copy fonts to their proper place

--- deploy/build-devblog.sh
+++ deploy/build-devblog.sh
@@ -3,3 +3,8 @@
 mkdir -p dist/blog
 
 cp -r devblog/public/* dist/blog
+# For reasons, the Hugo theme Gokarna's CSS style doesn't prepend /blog to the font URLs.
+# To remediate this, just copy the fonts to the root.
+
+mkdir -p dist/fonts
+cp dist/blog/fonts/* dist/fonts
