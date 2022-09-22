ca56dab4d0101b89fdda30d4b29a212914bed163
Daniel Chiquito <daniel.chiquito@gmail.com>
2022-09-21 21:24:36 -0400

    Rebuild

--- deploy/build-vue.sh
+++ deploy/build-vue.sh
@@ -3,4 +3,4 @@
 mkdir -p dist
 
 docker run --rm -v $(pwd)/goban-vue:/goban-vue -w /goban-vue -v $(pwd)/dist:/outDir node npm install && \
-docker run --rm -v $(pwd)/goban-vue:/goban-vue -w /goban-vue -v $(pwd)/dist:/outDir node npm run build-only -- --emptyOutDir --outDir /outDir --assetsDir static
+docker run --rm -v $(pwd)/goban-vue:/goban-vue -w /goban-vue -v $(pwd)/dist:/outDir node npm run build-only -- --outDir /outDir --assetsDir static
