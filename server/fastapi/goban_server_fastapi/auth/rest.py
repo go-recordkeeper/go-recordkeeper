32bccb84486c1139492b6f3487d1b79b74e5c778
Daniel Chiquito <daniel.chiquito@gmail.com>
2022-11-14 18:29:44 -0500

    Add openapi docs to fastapi auth endpoints

--- server/fastapi/goban_server_fastapi/auth/rest.py
+++ server/fastapi/goban_server_fastapi/auth/rest.py
@@ -26,8 +26,24 @@ class UserResponse(BaseModel):
     email: str
 
 
-@app.post("/api/login/", status_code=200)
+@app.post(
+    "/api/login/",
+    status_code=200,
+    response_model=str,
+    response_description="Fresh authentication token",
+    responses={401: {"description": "Authentication failed"}},
+)
 def login(login: LoginRequest, db: DbClient = Depends()):
+    """
+    Log a user in using the given username and password.
+
+    If successful, return an authentication token.
+    This token should be included in the `Authorization` header of any request that requires authentication, like so:
+
+    `Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6OCwiZXhwIjoxNjY4NTUzNzM3fQ.U_qsYxm88a7-e5laxk83Z6qx3y-mx6dmYXAYtmTCfnw`
+
+    On the docs page, this can be done by clicking "Authorize" in the top right corner and pasting in the token (without quotes).
+    """
     u = get_user(db, username=login.username)
     if u is not None:
         hasher = PBKDF2PasswordHasher()
@@ -36,8 +52,15 @@ def login(login: LoginRequest, db: DbClient = Depends()):
     return Response(status_code=401)
 
 
-@app.post("/api/register/", status_code=201, response_model=UserResponse)
+@app.post(
+    "/api/register/",
+    status_code=201,
+    response_model=UserResponse,
+    response_description="New user created successfully",
+    responses={400: {"description": "A user with that username already exists"}},
+)
 def register(register: RegisterRequest, db: DbClient = Depends()):
+    """Register a user with the given username, email, and password."""
     hasher = PBKDF2PasswordHasher()
     password_hash = hasher.encode(register.password, hasher.salt())
     new_user = create_user(
@@ -56,4 +79,5 @@ def register(register: RegisterRequest, db: DbClient = Depends()):
 
 @app.get("/api/user/", status_code=200, response_model=UserResponse)
 def user(current_user: User = Depends(jwt_user)):
+    """Get the currently logged in user, according to the `Authorization` header."""
     return current_user.__dict__
