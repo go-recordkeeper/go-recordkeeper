300598e647580677dff97cab9fabc59c2e323bc6
Daniel Chiquito <daniel.chiquito@gmail.com>
2022-11-13 20:57:53 -0500

    Partially repair deploy build scripts

--- deploy/build-devblog.sh
+++ deploy/build-devblog.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+cd ../blog
+hugo
+cd ../deploy
+
 mkdir -p dist/blog
 
-cp -r devblog/public/* dist/blog
+cp -r ../blog/public/* dist/blog
 # For reasons, the Hugo theme Gokarna's CSS style doesn't prepend /blog to the font URLs.
 # To remediate this, just copy the fonts to the root.
 
--- deploy/build-vue.sh
+++ deploy/build-vue.sh
@@ -2,5 +2,5 @@
 
 mkdir -p dist
 
-docker run --rm -v $(pwd)/goban-vue:/goban-vue -w /goban-vue -v $(pwd)/dist:/outDir node npm install && \
-docker run --rm -v $(pwd)/goban-vue:/goban-vue -w /goban-vue -v $(pwd)/dist:/outDir node npm run build-only -- --outDir /outDir --assetsDir static
+docker run --rm -v $(pwd)/../client/vue:/goban-vue -w /goban-vue -v $(pwd)/dist:/outDir node npm install && \
+docker run --rm -v $(pwd)/../client/vue:/goban-vue -w /goban-vue -v $(pwd)/dist:/outDir node npm run build-only -- --outDir /outDir --assetsDir static
