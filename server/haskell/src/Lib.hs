a72f1f6454d8d23d87ae3eb8321dc520359e46a3
Daniel Chiquito <daniel.chiquito@gmail.com>
2022-11-15 15:34:12 -0500

    haskell hello world

--- server/haskell/src/Lib.hs
+++ server/haskell/src/Lib.hs
@@ -1,6 +1,37 @@
-module Lib
-    ( someFunc
-    ) where
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeOperators #-}
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+module Lib (startApp, app) where
+
+import Data.Aeson
+import Data.Aeson.TH
+import Network.Wai
+import Network.Wai.Handler.Warp
+import Servant
+
+data User = User
+  { userId :: Int,
+    userFirstName :: String,
+    userLastName :: String
+  }
+  deriving (Eq, Show)
+
+$(deriveJSON defaultOptions ''User)
+
+type API = "users" :> Get '[JSON] [User]
+
+startApp :: IO ()
+startApp = run 8080 app
+
+app :: Application
+app = serve api server
+
+api :: Proxy API
+api = Proxy
+
+server :: Server API
+server = return users
+
+users :: [User]
+users = [User 1 "Daniel" "Chiquito"]
