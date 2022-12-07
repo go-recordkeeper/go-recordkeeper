6c5d16654b11a1e6d35adc712993e723bab8d321
Daniel Chiquito <daniel.chiquito@gmail.com>
2022-12-06 20:10:07 -0500

    add axum to rust

---
+++ server/rust/src/auth/mod.rs
@@ -0,0 +1,53 @@
+use axum::{
+    async_trait,
+    extract::FromRequestParts,
+    http::{header::AUTHORIZATION, request::Parts, HeaderValue, StatusCode},
+    routing::{get, post},
+    Router,
+};
+
+struct ExtractAuthorizationBearer(String);
+
+#[async_trait]
+impl<S> FromRequestParts<S> for ExtractAuthorizationBearer
+where
+    S: Send + Sync,
+{
+    type Rejection = (StatusCode, &'static str);
+
+    async fn from_request_parts(parts: &mut Parts, _state: &S) -> Result<Self, Self::Rejection> {
+        parts
+            .headers
+            .get(AUTHORIZATION)
+            .map(HeaderValue::to_str)
+            .map(Result::ok)
+            .flatten()
+            .map(|auth| auth.strip_prefix("Bearer "))
+            .flatten()
+            .map_or(
+                Err((StatusCode::BAD_REQUEST, "Failed to authenticate")),
+                |token| Ok(ExtractAuthorizationBearer(token.to_string())),
+            )
+    }
+}
+
+async fn login() {
+    println!("loggin in");
+}
+
+async fn register() {
+    println!("Registering");
+}
+
+async fn get_current_user(ExtractAuthorizationBearer(foo): ExtractAuthorizationBearer) {
+    println!("gettin current user");
+    println!("{}", foo);
+}
+
+/// Define all the routes
+pub fn register_routes(router: Router) -> Router {
+    router
+        .route("/api/login/", post(login))
+        .route("/api/register/", post(register))
+        .route("/api/user/", get(get_current_user))
+}
--- server/rust/src/main.rs
+++ server/rust/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    routing::get,
+    Router,
+};
+
+mod auth;
+
+#[tokio::main]
+async fn main() {
+    // build our application with a single route
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = auth::register_routes(app);
+
+    // run it with hyper on localhost:3000
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
