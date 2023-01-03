2f0b9ed3322e5bad865741de16a221c789f28daf
Daniel Chiquito <daniel.chiquito@gmail.com>
2023-01-03 11:00:52 -0500

    haskell: hash passwords

--- server/haskell/src/Auth/Register.hs
+++ server/haskell/src/Auth/Register.hs
@@ -3,10 +3,12 @@
 module Auth.Register (register) where
 
 -- import Auth.User
+-- import Auth.User
 import Control.Monad.IO.Class (liftIO)
 import Data.Aeson.TH (defaultOptions, deriveJSON)
 import qualified Data.ByteString.Char8 as BS
 import Data.Int (Int64)
+import Data.Password.PBKDF2 (PBKDF2Algorithm (PBKDF2_SHA256), PBKDF2Params (PBKDF2Params, pbkdf2Algorithm, pbkdf2Iterations, pbkdf2OutputLength, pbkdf2Salt), hashPasswordWithParams, mkPassword)
 import Data.Text (Text, pack)
 import qualified Data.Text.Lazy as Lazy
 import Data.Time (UTCTime)
@@ -57,15 +59,21 @@ insert =
     ($1 :: text, $2 :: text, $3 :: text, $4 :: timestamptz, $4 :: timestamptz, '', '', false, false, false)
   |]
 
+hashPassword :: String -> IO String
+hashPassword x = do
+  let params = PBKDF2Params {pbkdf2Salt = 16, pbkdf2Algorithm = PBKDF2_SHA256, pbkdf2Iterations = 390000, pbkdf2OutputLength = 64}
+  hash <- hashPasswordWithParams params $ mkPassword $ pack x
+  return $ show hash
+
 register :: HP.Pool -> ScottyM ()
 register pool = post "/api/register/" $ do
   RegisterRequest {username, email, password} <- jsonData :: ActionM RegisterRequest
   if not $ isValid $ BS.pack email
     then raiseStatus status400 "Invalid email"
     else do
-      -- TODO hash the password
+      passwordHash <- liftAndCatchIO $ hashPassword password
       now <- liftAndCatchIO Clock.getCurrentTime
-      let sess = HS.statement (pack username, pack email, pack password, now) insert
+      let sess = HS.statement (pack username, pack email, pack passwordHash, now) insert
       result <- liftIO $ HP.use pool sess
       case result of
         Right id' -> do
