9890c2894a1ba0cffa0b33476800ebcccc671cfd
Daniel Chiquito <daniel.chiquito@gmail.com>
2023-02-01 13:58:50 -0500

    Use env vars for DB connection

--- server/haskell/src/Lib.hs
+++ server/haskell/src/Lib.hs
@@ -1,23 +1,25 @@
-{-# LANGUAGE OverloadedStrings #-}
-
 module Lib (startApp) where
 
 import Auth (authEndpoints)
+import qualified Data.ByteString.UTF8 as BSU
 import GHC.IO.Handle (hFlush)
 import GHC.IO.Handle.FD (stdout)
 import qualified Hasql.Connection as HC
 import qualified Hasql.Pool as HP
 import Network.Wai.Middleware.RequestLogger (logStdout)
 import Record (recordEndpoints)
+import System.Environment (getEnv)
 import Web.Scotty (middleware, scotty)
 
 startApp :: IO ()
 startApp = do
   putStrLn "Starting the server..."
   hFlush stdout
-  -- let connectionSettings = HC.settings "localhost" 5432 "postgres" "postgres" "default"
-  -- TODO test configuration only
-  let connectionSettings = HC.settings "postgres" 5432 "postgres" "postgres" "default"
+  dbName <- BSU.fromString <$> getEnv "POSTGRES_NAME"
+  dbUser <- BSU.fromString <$> getEnv "POSTGRES_USER"
+  dbPassword <- BSU.fromString <$> getEnv "POSTGRES_PASSWORD"
+  dbHost <- BSU.fromString <$> getEnv "POSTGRES_HOST"
+  let connectionSettings = HC.settings dbHost 5432 dbUser dbPassword dbName
   pool <- HP.acquire (10, 30, connectionSettings)
   scotty 8000 $ do
     middleware logStdout
