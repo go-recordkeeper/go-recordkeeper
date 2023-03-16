f143ea8af467ce6313ea6d2efb8f712a2ddd586d
Daniel Chiquito <daniel.chiquito@gmail.com>
2023-03-15 20:16:12 -0400

    Remove old code to trigger workflows

--- test/goban_server_test/main.py
+++ test/goban_server_test/main.py
@@ -4,8 +4,4 @@ import pytest
 
 
 def run():
-    # print(sys.argv[1:])
-    # process = Popen(["docker", "compose", "run", "--rm", "django", "python", "manage.py", "migrate"])
-    # print(process.wait())
-    # Invoke pytest with any arguments passed
     print(pytest.main(["goban_server_test"] + sys.argv[1:]))
