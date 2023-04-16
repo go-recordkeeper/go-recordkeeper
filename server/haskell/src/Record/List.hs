04cee697d8f97e50bd3089f35f89311547655a35
Daniel Chiquito <daniel.chiquito@gmail.com>
2023-04-16 10:47:58 -0400

    Paginate haskell

--- server/haskell/src/Record/List.hs
+++ server/haskell/src/Record/List.hs
@@ -1,24 +1,29 @@
 module Record.List (list) where
 
 import Auth.JWT (authorizedUserId)
+import Control.Monad (when)
 import DB (execute)
 import Data.Aeson.TH (defaultOptions, deriveJSON)
 import Data.Int (Int64)
 import qualified Data.Text as T
 import Data.Time (UTCTime)
+import Data.Vector (Vector)
 import qualified Data.Vector as V
 import qualified Hasql.Pool as HP
 import qualified Hasql.Statement as S
 import qualified Hasql.TH as TH
-import Network.HTTP.Types (status200)
+import Network.HTTP.Types (status200, status404)
 import Web.Scotty
   ( ScottyM,
     get,
     json,
+    param,
+    raiseStatus,
+    rescue,
     status,
   )
 
-data ListResponse = GetResponse
+data RecordResponse = RecordResponse
   { id :: Int,
     owner :: Int,
     board_size :: Int,
@@ -34,11 +39,30 @@ data ListResponse = GetResponse
   }
   deriving (Eq, Show)
 
+$(deriveJSON defaultOptions ''RecordResponse)
+
+data ListResponse = ListResponse
+  { count :: Int,
+    pages :: Int,
+    results :: Vector RecordResponse
+  }
+  deriving (Eq, Show)
+
 $(deriveJSON defaultOptions ''ListResponse)
 
 type RecordRow = (Int64, Int64, T.Text, T.Text, T.Text, T.Text, Int64, Double, T.Text, T.Text, UTCTime)
 
-select :: S.Statement Int64 (V.Vector RecordRow)
+countStatement :: S.Statement Int64 Int64
+countStatement =
+  [TH.singletonStatement|
+    select
+    count(*) :: int8
+    from record_record
+    where
+    owner_id = $1 :: int8
+  |]
+
+select :: S.Statement (Int64, Int64, Int64) (V.Vector RecordRow)
 select =
   [TH.vectorStatement|
     select
@@ -47,11 +71,13 @@ select =
     where
     owner_id = $1 :: int8
     order by created desc
+    limit $2 :: int8
+    offset $3 :: int8
   |]
 
-toResponse :: Int64 -> RecordRow -> ListResponse
+toResponse :: Int64 -> RecordRow -> RecordResponse
 toResponse userId (recordId, board_size, name, black_player, white_player, comment, handicap, komi, ruleset, winner, created) =
-  GetResponse
+  RecordResponse
     { id = fromIntegral recordId,
       owner = fromIntegral userId,
       board_size = fromIntegral board_size,
@@ -69,6 +95,18 @@ toResponse userId (recordId, board_size, name, black_player, white_player, comme
 list :: HP.Pool -> ScottyM ()
 list pool = get "/api/records/" $ do
   userId <- authorizedUserId
-  rows <- execute pool select userId
+  pageSize <- param "page_size" `rescue` (\_ -> return 10)
+  page <- param "page" `rescue` (\_ -> return 1)
+  when (pageSize < 1) $ raiseStatus status404 "Invalid page size"
+  totalRecords <- execute pool countStatement userId
+  let pages' = totalRecords `div` pageSize
+      pages = if pages' == 0 then 1 else pages'
+  when (page < 1 || page > pages) $ raiseStatus status404 "Invalid page number"
+  rows <- execute pool select (userId, pageSize, (page - 1) * pageSize)
   status status200
-  json $ V.map (toResponse userId) rows
+  json $
+    ListResponse
+      { count = fromIntegral totalRecords,
+        pages = fromIntegral pages,
+        results = V.map (toResponse userId) rows
+      }
