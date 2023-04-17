67aa6e225424977b4ac52549376b9ed5e3a85f53
Daniel Chiquito <daniel.chiquito@gmail.com>
2023-04-17 15:48:01 -0400

    Allow all CORS in development

--- server/rust/src/main.rs
+++ server/rust/src/main.rs
@@ -1,6 +1,8 @@
 use std::sync::Arc;
 
+use axum::http::{header, Method};
 use axum::Router;
+use tower_http::cors::{Any, CorsLayer};
 
 mod auth;
 mod db;
@@ -13,6 +15,17 @@ async fn main() {
     let app = Router::new();
     let app = auth::register_routes(app).with_state(client.clone());
     let app = record::register_routes(app).with_state(client.clone());
+    let app = if std::env::var("GOBAN_DEVELOPMENT").is_ok() {
+        app.route_layer(
+            CorsLayer::new()
+                .allow_methods([Method::GET, Method::OPTIONS, Method::POST, Method::PUT])
+                .allow_origin(Any)
+                .allow_headers([header::CONTENT_TYPE, header::AUTHORIZATION])
+                .expose_headers([header::CONTENT_DISPOSITION]),
+        )
+    } else {
+        app
+    };
 
     println!("Starting server...");
     axum::Server::bind(&"0.0.0.0:8000".parse().unwrap())
