ade73c997ab7613b3310d3ad2cad4ac9eaaf40ca
Daniel Chiquito <daniel.chiquito@gmail.com>
2023-05-10 17:31:08 -0400

    Rudimentary JSON validation

--- server/deno/src/auth/login.ts
+++ server/deno/src/auth/login.ts
@@ -1,14 +1,21 @@
 import { register } from "/router.ts";
-import { postgres } from "/deps.ts";
+import { sql } from "/db.ts";
+import { ajv, json_schema as J } from "/deps.ts";
 
-// Deno.env.get("POSTGRES_USER")
-// const sql = postgres("postgres://username:password@host:port/database");
+// type LoginRequest = { username: string; password: string };
+const LR = J.struct({ username: J.string(), password: J.string() });
+
+const validate = ajv.compile(J.print(LR));
 
 register(
   "POST",
   "/api/login/",
   async (request, params) => {
-    console.log("fooo", await request.json());
+    let json: J.TypeOf<typeof LR> = await request.json();
+    console.log(json);
+    console.log(validate(json));
+    let x = await sql`SELECT id, password, is_active FROM auth_user;`;
+    console.log(x);
     return new Response("aaa", { status: 200 });
   },
 );
--- server/deno/src/deps.ts
+++ server/deno/src/deps.ts
@@ -1,1 +1,6 @@
 export { default as postgres } from "https://deno.land/x/postgresjs@v3.3.4/mod.js";
+export * as json_schema from "https://deno.land/x/fun@v.2.0.0-alpha.11/json_schema.ts";
+
+// idk what's up with this hideousness
+import Ajv from "npm:ajv@8.12.0";
+export const ajv = new Ajv.default();
