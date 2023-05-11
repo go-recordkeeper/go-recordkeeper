65a30b2a7241bde8eb7b07a3826cad0557199d70
Daniel Chiquito <daniel.chiquito@gmail.com>
2023-05-10 23:32:38 -0400

    Technically functional password hashing

--- server/deno/src/auth/login.ts
+++ server/deno/src/auth/login.ts
@@ -10,9 +10,48 @@ register(
   "POST",
   "/api/login/",
   async (request, params) => {
-    let json = await body(request);
-    let x = await sql`SELECT id, password, is_active FROM auth_user;`;
+    const json = await body(request);
+    const x =
+      await sql`SELECT id, password, is_active FROM auth_user WHERE username=${json.username};`;
     console.log(x);
-    return new Response("aaa", { status: 200 });
+    const dbthingy: string = x[0].password;
+    const match = dbthingy.match(
+      /^pbkdf2_sha256\$([0-9]+)\$([a-zA-Z0-9+/]+)\$([a-zA-Z0-9+/=]+)$/,
+    );
+    if (!match) {
+      // TODO
+      throw "bad juju";
+    }
+    const [_, iterations, salt, hash] = match;
+    const passkey: CryptoKey = await crypto.subtle.importKey(
+      "raw",
+      new TextEncoder().encode(json.password),
+      { name: "PBKDF2" },
+      false,
+      ["deriveKey", "deriveBits"],
+    );
+    const kk = await crypto.subtle.deriveKey(
+      {
+        name: "PBKDF2",
+        salt: new TextEncoder().encode(salt),
+        iterations: parseInt(iterations, 10),
+        hash: "SHA-256",
+      },
+      passkey,
+      { name: "HMAC", hash: "SHA-256", length: 32 * 8 },
+      true,
+      ["sign", "verify"],
+    );
+    const out = await crypto.subtle.exportKey("raw", kk);
+    const buf2b64 = (buffer: ArrayBuffer) =>
+      self.btoa(
+        Array.prototype.map.call(
+          new Uint8Array(buffer),
+          (x) => String.fromCharCode(x),
+        ).join(""),
+      );
+    console.log("computed", buf2b64(out));
+    console.log("from db ", hash);
+    return new Response("" + (buf2b64(out) == hash), { status: 200 });
   },
 );
