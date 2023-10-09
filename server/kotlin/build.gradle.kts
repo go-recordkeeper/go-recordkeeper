8612276828b229449c5f90153bac202cc35262eb
Daniel Chiquito <daniel.chiquito@gmail.com>
2023-10-09 11:59:00 -0400

    Start Kotlin

---
+++ server/kotlin/build.gradle.kts
@@ -0,0 +1,33 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+  id("org.springframework.boot") version "3.1.4"
+  id("io.spring.dependency-management") version "1.1.3"
+  kotlin("jvm") version "1.9.10"
+  kotlin("plugin.spring") version "1.9.10"
+}
+
+group = "com.chiquito.recordkeeper"
+
+version = "0.0.1-SNAPSHOT"
+
+java { sourceCompatibility = JavaVersion.VERSION_17 }
+
+repositories { mavenCentral() }
+
+dependencies {
+  implementation("org.springframework.boot:spring-boot-starter-web")
+  implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+  implementation("org.jetbrains.kotlin:kotlin-reflect")
+  runtimeOnly("org.postgresql:postgresql")
+  testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<KotlinCompile> {
+  kotlinOptions {
+    freeCompilerArgs += "-Xjsr305=strict"
+    jvmTarget = "17"
+  }
+}
+
+tasks.withType<Test> { useJUnitPlatform() }
---
+++ server/kotlin/src/main/kotlin/com/chiquito/recordkeeper/kotlin/KotlinApplication.kt
@@ -0,0 +1,10 @@
+package com.chiquito.recordkeeper.kotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication class KotlinApplication
+
+fun main(args: Array<String>) {
+  runApplication<KotlinApplication>(*args) {}
+}
