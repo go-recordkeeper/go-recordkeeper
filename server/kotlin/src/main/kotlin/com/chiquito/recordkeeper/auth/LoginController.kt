479ba09f7557fca3a60a085f775efa7949a1bce1
Daniel Chiquito <daniel.chiquito@gmail.com>
2023-10-15 18:34:02 -0400

    Generate JWT

--- server/kotlin/src/main/kotlin/com/chiquito/recordkeeper/auth/LoginController.kt
+++ server/kotlin/src/main/kotlin/com/chiquito/recordkeeper/auth/LoginController.kt
@@ -1,8 +1,14 @@
 package com.chiquito.recordkeeper.auth
 
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
 import io.github.oshai.kotlinlogging.KotlinLogging
+import kotlin.time.DurationUnit
+import kotlin.time.toDuration
+import kotlinx.datetime.Clock
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
 
 private val logger = KotlinLogging.logger {}
@@ -13,8 +19,25 @@ class LoginController {
   data class Request(val username: String, val password: String)
 
   @PostMapping("/api/login/")
-  fun login(@RequestBody x: Request): String {
-    logger.debug { "logging in as $x" }
-    return "\"jwt token for real trust me\""
+  @ResponseBody
+  fun login(@RequestBody request: Request): String {
+    val (username, password) = request
+    // TODO hook up DB
+    // TODO pbkdf2 to check password
+    val now = Clock.System.now()
+    val jwt =
+        JWT.create()
+            .withClaim("sub", "666")
+            .withClaim("iat", now.epochSeconds)
+            .withClaim("exp", (now + 1.toDuration(DurationUnit.DAYS)).epochSeconds)
+            .withClaim("iss", "go-recordkeeper")
+            .withClaim("aud", "go-recordkeeper")
+            // TODO grab secret key from env config
+            .sign(
+                Algorithm.HMAC256(
+                    "django-insecure-(@ppnpk\$wx_z%2^#^0sext&+%b58=%e^!_u_*yd2p#d2&9)9cj"
+                )
+            )
+    return "\"$jwt\""
   }
 }
